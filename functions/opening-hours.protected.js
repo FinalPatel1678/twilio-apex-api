631fe3f4144d47c1e4d31b4aae5b7990a3434fd1
final-patel1603 <fpatel@in.allmysons.com>
2025-03-20 08:47:24 +0530

    init apex serverless functions

---
+++ functions/opening-hours.protected.js
@@ -0,0 +1,23 @@
+const moment = require("moment-timezone");
+
+exports.handler = function (context, event, callback) {
+    // Get the current time in Pacific Time (PST/PDT automatically adjusted)
+    const currentTime = moment().tz("America/Los_Angeles");
+
+    // Extract the hour
+    const currentHour = currentTime.hour();
+
+    // Define business hours (9 AM - 5 PM PST, GMT-7)
+    const startHour = 9;
+    const endHour = 17;
+
+    let response = {
+        inhours: false // Default to false (after hours)
+    };
+
+    if (currentHour >= startHour && currentHour < endHour) {
+        response.inhours = true; // 9 AM - 5 PM (Business hours)
+    }
+
+    callback(null, response);
+};
